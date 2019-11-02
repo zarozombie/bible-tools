5c9d6d07be3650daf76c7c77212b603e876f9054
Lazaro Carroll <lazarocarroll@gmail.com>
2019-11-02 14:46:53 -0400

    working on regular expression to parse specific verses i.e gen 3:5-6

--- bible_reader/string_filter/sting.py
+++ bible_reader/string_filter/sting.py
@@ -3,7 +3,7 @@ bible_string = input("enter bible string: ")
 parse_string = []
 
 #regular expresion used to parse data
-parse_string = re.split(' |:', bible_string)
+parse_string = re.split(' |:|-', bible_string)
 
 print(parse_string)
 

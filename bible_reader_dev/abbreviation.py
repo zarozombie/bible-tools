1dbbcd17c1f8fb894a6008a4b7091748ffaf6eed
Lazaro Carroll <lazarocarroll@gmail.com>
2019-11-09 12:05:42 -0500

    split reader and dev

---
+++ bible_reader_dev/abbreviation.py
@@ -0,0 +1,151 @@
+#abbreviation
+
+def shorthand(short):
+    if short == "isa"    or short == "isaiah":
+        return"Isaiah"
+    if short == "gen" or short == "genesis":
+        return"Genesis"
+    elif short == "luk" or short == "luke":
+        return "Luke"
+    if short == "mat" or short == "matt" or short == "matthew":
+        return "Matthew"
+    if short == "1chr" or short == "1 chronicles":
+        return"1 Chronicles"
+    if short == "1cor" or short == "1 co" or short == "1co" or short == "1 corinthians" :
+        return"1 Corinthians"
+    if short == "1 joh" or short == "1 john" or short == "1jn" or short == "1 jn":
+        return "1 John"
+    if short == "1ki" or short == "1 kings":
+        return "1 Kings"
+    if short == "1 pe" or short == "1pe" or short == "1 peter":
+        return "1 Peter"
+    if short == "1 samuel" or short == "1sa" or short == "1 sa":
+        return"1 Samuel"
+    if short == "1 thessalonians":
+        return "1 Thessalonians"
+    if short == "1 timothy":
+        return "1 Timothy"
+    if short == "2 chronicles" or short == "2ch":
+        return"2 Chronicles"
+    if short == "2 corinthians" or short == "2co":
+        return"2 Corinthians"
+    if short == "2 john" or short == "2jn" or short == "2 jn":
+        return "2 John"
+    if short == "2 kings":
+        return "2 Kings"
+    if short == "2 peter":
+        return "2 Peter"
+    if short == "2 sam" or short == "2 samuel" or short == "2sam" or short == "2sa" or short == "2 sa":
+        return "2 Samuel"        
+    if short == "2 thessalonians" or short == "2th" or short == "2 th":
+        return "2 Thessalonians"
+    if short == "2 ti" or short == "2 timothy":
+        return "2 Timothy"
+    if short == "3 john":
+        return "3 John"
+    if short == "act" or short == "acts":
+        return "Acts"        
+    if short == "amos":
+        return "Amos"
+    if short == "colossians":
+        return "Colossians"
+    if short == "dan" or short == "daniel":
+        return "Daniel"
+    if short == "deuteronomy" or short == "deu":
+        return "Deuteronomy"
+    if short == "ecclesiastes":
+        return "Ecclesiastes"
+    if short == "eph" or short == "ephesian":
+        return "Ephesians"
+    if short == "esther":
+        return "Esther"
+    if short == "exo" or short == "exodus":
+        return "Exodus"
+    if short == "ezekiel" or short == "eze":
+        return "Ezekiel"
+    if short == "ezra":
+        return "Ezra"
+    if short == "galatian" or short == "gal":
+        return "Galatians"
+    if short == "habakkuk":
+        return "Habakkuk"
+    if short == "haggai" or short == "hag":
+        return "Haggai"
+    if short == "hebrews" or short == "heb":
+        return "Hebrews"
+    if short == "hos" or short == "hosea":
+        return "Hosea"
+    if short == "james" or short == "jas":
+        return "James"
+    if short == "jer" or short == "jeremiah":
+        return "Jeremiah"
+    if short == "job":
+        return "Job"
+    if short == "joe" or short == "joel":
+        return "Joel"
+    if short == "jon" or short == "joh" or short == "john":
+        return "John"
+    if short == "jonah":
+        return "Jonah"
+    if short == "jos" or short == "joshua":
+        return "Joshua"
+    if short == "jude" or short == "jud":
+        return "Jude"
+    if short == "judges" or short == "jdg":
+        return "Judges"
+    if short == "lamentations" or short == "lam":
+        return "Lamentations"
+    if short == "lev" or short == "leviticus":
+        return "Leviticus"
+    if short == "malachi" or short == "mal":
+        return "Malachi"
+    if short == "mark" or short == "mar":
+        return "Mark"
+    if short == "micah":
+        return "Micah"
+    if short == "nahum":
+        return "Nahum"
+    if short == "nehemiah":
+        return "Nehemiah"
+    if short == "numbers" or short == "num":
+        return "Numbers"
+    if short == "obadiah":
+        return "Obadiah"
+    if short == "philemon":
+        return "Philemon"
+    if short == "php" or short == "philippians":
+        return "Philippians"
+    if short == "proverbs" or short == "pro":
+        return "Proverbs"
+    if short == "psa" or short == "paslms":
+        return "Psalms"
+    if short == "rev" or short == "revelation":
+        return "Revelation"
+    if short == "romans" or short == "rom":
+        return "Romans"
+    if short == "ruth":
+        return "Ruth"    
+    if short == "sirach" or short == "sir":
+        return "Sirach"
+    if short == "song" or short == "song of solomon" or short == "son":
+        return "Song of Solomon"
+    if short == "tit" or short == "titus":
+        return "Titus"
+    if short == "tob" or short == "tobit": 
+        return "Tobit"
+    if short == "wisdom" or short == "wis":
+        return "Wisdom"
+    if short == "zechariah" or short == "zec":
+        return "Zechariah"
+    if short == "zephaniah":
+        return "Zephaniah"
+    else:
+        return "book or abrev not found!"
+
+
+# x = "zephaniah"
+
+# ans = shorthand(x.lower())
+# ans = shorthand("rev")
+# print(ans)
+# print("gen")
---
+++ bible_reader_dev/main.py
@@ -0,0 +1,53 @@
+# modules
+import abbreviation
+import bible_class
+import re
+# Modules included but not needed possibally remove next revision
+#import titles
+
+"""
+------------------------------------------------------------------Book Reading algorythm------------------------------------------
+this portion of code is what opens books by asking user for input for specific Book (book_abrev) and Book Verse (verse) and displays scriptuer by Book and verse
+"""
+
+#flag to exit program
+done = False
+
+def main(book_loc):
+    parsed_array = []
+
+    global done
+    if done == True:
+        exit()
+    else:
+
+
+#  ==============  print Titles - Needs revision to print Books in a better format ==============
+#        if book_loc == "./KJVA":
+#             titles.kjv()
+#        elif book_loc == "./RSV":
+#             titles.rsv()
+
+# Enter Book and verse and return Book Verse
+        print("-----"*10 , "Welcome" , "-----"*10)
+        book_abrev = input("What do you want to look for? ")        
+        parsed_array = re.split(' |:|-', book_abrev)
+        if parsed_array == 'q':
+            done = True
+            main(book_loc)
+        else:
+            book_name = abbreviation.shorthand(parsed_array[0].lower())
+            try:
+                bible_ver = bible_class.Book(book_name, parsed_array[1:], "", "", "", "")
+            except:
+                bible_ver = bible_class.Book(book_name, parsed_array, "", "", "", "")
+
+#-------------------------------------------------------------------
+
+            book_txt = bible_ver.format_bk()                                                      #add .txt to end of file
+            bible_str = bible_ver.open_bk(book_txt, book_loc)                                           #open file in string
+            ch_index = bible_ver.create_ch_idex()                                              #crate verse index
+            print(ch_index)
+            verse_index = bible_ver.index_vrs(bible_str, ch_index[0], ch_index[1])              #index verse
+            bible_ver.print_verse(bible_str, verse_index[0], verse_index[1], book_loc)                #display verse
+
